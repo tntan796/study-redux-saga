1ac86bf2d023b16e5d550a201c6e82e71fe684bc
Tan Tano <tntan796@gmail.com>
2021-02-02 22:35:04 +0700

    config saga

--- src/redux/configure-store.js
+++ src/redux/configure-store.js
@@ -1,18 +1,24 @@
 import { applyMiddleware, compose, createStore } from "redux";
 import rootReducer from '../reducers/root.reducer';
 import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from '../sagas/root.saga';
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const sagaMiddleware = createSagaMiddleware();
 
 const configureStore = () => {
     const middlewares = [
-        thunk
+        thunk, sagaMiddleware
     ];
     
     const enhancers = [
         applyMiddleware(...middlewares)
     ];
     const store = createStore(rootReducer, composeEnhancers(...enhancers));
+    sagaMiddleware.run(rootSaga);
     return store;
 }
+
 
 export default configureStore;
---
+++ src/sagas/root.saga.js
@@ -0,0 +1,8 @@
+import {fork, take} from 'redux-saga/effects';
+import { ACTION_TYPES } from '../common/contants';
+
+function* rootSaga() {
+    yield true;
+}
+
+export default rootSaga;
